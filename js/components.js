6da8edce89d07d06ea5b6a4cf691aae8463df51e
robThot <robthot@gmail.com>
2012-03-20 19:22:47 +0100

    refact - tab/space

--- js/components.js
+++ js/components.js
@@ -1,29 +1,29 @@
 var wm = {};
 
 wm.Component = function(config){
-	this.init(config);
+  this.init(config);
 };
 
 wm.Grid = $.componentExtend(wm.Component, {
 
-	type: 'grid',
+  type: 'grid',
 
-	dataStore: {},
+  dataStore: {},
 
-	init: function(config) {
-		console.log('masodik');
-		console.log(config);
-		this.build();
-	},
+  init: function(config) {
+    console.log('masodik');
+    console.log(config);
+    this.build();
+  },
 
-	build: function() {
-		$.createEl({
-			type  : 'table',
-			id    : 'test',
-			cls   : 'anyClass',
-			style : 'width:50px;height:50px;background:red;position:absolute;top:0px;left:0px;'
-		});
-	}
+  build: function() {
+    $.createEl({
+      type  : 'table',
+      id    : 'test',
+      cls   : 'anyClass',
+      style : 'width:50px;height:50px;background:red;position:absolute;top:0px;left:0px;'
+	});
+  }
 });
 
 // vm.DataStore = $.extend(wm.Component, {

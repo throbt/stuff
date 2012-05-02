78f0c7199481578ffafdb669de9a1a074ca686cc
robThot <robthot@gmail.com>
2012-05-02 14:53:36 +0200

    saving

--- application/controllers/test/test.php
+++ application/controllers/test/test.php
@@ -1,19 +1,19 @@
 <?php
 
 class Test extends Controller {
-	public function init() {
-		echo 'this is the controller init' . "<br />";
-	}
-	
-	public function index() {
-		echo 'index';
-	}
-	
-	public function show() {
-		echo "show:  id={$this->router->params->index}";
-	}
-	
-	public function thisTest() {
-		echo 'thisTest';
-	}
+  public function init() {
+  	echo 'this is the controller init' . "<br />";
+  }
+  
+  public function index() {
+  	echo 'index';
+  }
+  
+  public function show() {
+  	echo "show:  id={$this->router->params->index}";
+  }
+  
+  public function thisTest() {
+  	echo 'thisTest';
+  }
 }
